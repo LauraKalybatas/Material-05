5e0586dbdaec089b7e7e9e858f320fa075162828
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-11 20:50:27 -0300

    exercicio 1 completo

---
+++ src/exercicio1/MonitorEnergia.java
@@ -0,0 +1,34 @@
+package exercicio1;
+
+public class MonitorEnergia {
+    double[] consumo;
+    int indice;
+
+    public MonitorEnergia(int tamanho) {
+        this.consumo = new double[tamanho];
+    }
+
+    public void registrar(double valor) {
+        if(indice < consumo.length){
+            consumo[indice] = valor;
+            indice++;
+        }
+    }
+
+    public double calcularMedia() {
+        double media = 0;
+        for(double valor : consumo){
+            media += valor;
+        }
+        return media / consumo.length;
+        //codigo pode ser substituido chamando o outro metodo de mesmo nome. evita repetir codigo
+    }
+
+    public double calcularMedia(int inicio, int fim) {
+        double media = 0;
+        for(int i = inicio; i <= fim; i++) {
+            media += consumo[i];
+        }
+        return media / (fim - inicio);
+    }
+}
